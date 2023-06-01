4fccdf04bd249f02a11b3e9e21cf4535a8cba684
joserenter1a <josevt209@gmail.com>
2023-06-01 12:52:15 -0700

    recur sols

--- recursion/recurL2_sol.py
+++ recursion/recurL2_sol.py
@@ -6,7 +6,7 @@ for CS211 Class Encore
 Find the max value in a matrix, recursively
 
 This problem is slightly trickier, but definitely fair game,
-as it combines two of the major concepts in this course,
+as it combines two of the major concepts in this course, 
 in 2D arrays or matrices and recursion
 
 Reference
@@ -22,29 +22,31 @@ def find_matrix_max(matrix):
 
 def matrix_max(rows, cols, m):
     # if rows out of bounds
-        # return tuple of Nones
     if rows == len(m):
+        # return tuple of Nones
         return None, None
     # if we reached the end of the row
     if cols == len(m[rows]):
-        return matrix_max(rows+1, 0, m)
         # go down to the start of the next row
-    max_row, max_col = matrix_max(rows, cols+1, m)
-    # recursively find the max moving to the next column
+        return matrix_max(rows + 1, 0, m)
+    # recursively move through the row,
+    # assigning the max row,col
+    max_row, max_col = matrix_max(rows, cols + 1, m)
     # if the max row is None
     if max_row is None:
+        # assign the max row,col
         max_row, max_col = rows, cols
-        # the current row,col is the max row,col
+    # if current row, col is greater than max row,col
     if m[rows][cols] > m[max_row][max_col]:
+        # reassign the max row,col
         max_row, max_col = rows, cols
-    # if the current row,col is greater than the current max row,col
-    # reassign max row,col
-    # return max_row, max_col
+    # return max row,col
     return max_row, max_col
 
 
 def test_matrix_max():
-    matrix = [[1, 9, 6], [8, 3, 7]]
+    matrix = [[1, 9, 6],
+              [8, 3, 7]]
     res = find_matrix_max(matrix)
     print(f'The max of the matrix can be found at {res}')
 
